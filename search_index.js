a53d8ca6fc85ef8a884947f24649223c6639f1fa
Documenter.jl <documenter@juliadocs.github.io>
2023-03-30 15:49:41 +0000

    build based on 7a11389

---
+++ search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#MultiTaskLearning.jl","page":"MultiTaskLearning.jl","title":"MultiTaskLearning.jl","text":"","category":"section"},{"location":"","page":"MultiTaskLearning.jl","title":"MultiTaskLearning.jl","text":"Documentation for MultiTaskLearning.jl","category":"page"}]
+}
